ead77e96f1c718e6f3400abdbece1b4843b4696f
sahra <sahra.hi80@gmail.com>
2022-01-06 00:35:31 +0330

    Added closed Event

--- main.cpp
+++ main.cpp
@@ -1,9 +1,40 @@
 //Snake Game with SFML
-
+#include <SFML/Graphics/CircleShape.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Graphics/Texture.hpp>
+#include <SFML/Window/Event.hpp>
+#include <SFML/Graphics.hpp>
 #include <iostream>
+
 using namespace std;
 
 int main() {
+    sf::ContextSettings settings;
+    settings.antialiasingLevel = 8;
+
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Snake Game", sf::Style::Default, settings);
+    window.setFramerateLimit(60);
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            switch (event.type) {
+                case sf::Event::Closed:
+                    window.close();
+                    break;
+                case sf::Event::KeyReleased:
+                    if (event.key.code == sf::Keyboard::Escape)
+                        window.close();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        window.clear();
+        // window.draw();
+        window.display();
+    }
 
     return 0;
 }
