211eb616a44466923f0f185bba5bf9b92102039d
sahra <sahra.hi80@gmail.com>
2022-01-20 17:37:14 +0330

    Added pause

--- PauseGame.cpp
+++ PauseGame.cpp
@@ -1,5 +1,49 @@
-//
-// Created by sama on 1/20/2022.
-//
-
 #include "PauseGame.h"
+
+PauseGame::PauseGame() {
+
+    if (!font.loadFromFile("calibri.ttf")) {
+        cout << "Error loading file" << endl;
+        system("pause");
+    }
+
+    //Change  Welcome text features.
+    pause.setFont(font);
+    pause.setFillColor(sf::Color::White);
+    pause.setString("Pause");
+    pause.setCharacterSize(50);
+    pause.setPosition(sf::Vector2f((800 / 2)-60, (600 / 2)-50 ));
+
+}
+
+PauseGame::~PauseGame() {
+
+}
+
+void PauseGame::drawPaus(sf::RenderWindow &window) {
+    while (window.isOpen()) {
+        sf::Event event{};
+        while (window.pollEvent(event)) {
+            switch (event.type) {
+                case sf::Event::KeyReleased:
+                    switch (event.key.code) {
+                        case sf::Keyboard::Space:
+                            goto unPause;
+                    }
+                    break;
+
+                    //To close the window.
+                case sf::Event::Closed:
+                    window.close();
+                    break;
+
+                default:
+                    break;
+            }
+        }
+        window.draw(pause);
+        window.display();
+
+    }
+    unPause:;
+}
--- PauseGame.h
+++ PauseGame.h
@@ -1,14 +1,24 @@
-//
-// Created by sama on 1/20/2022.
-//
+#ifndef PROJECT1_PAUSEGAME_H
+#define PROJECT1_PAUSEGAME_H
 
-#ifndef INC_1_PAUSEGAME_H
-#define INC_1_PAUSEGAME_H
+#include <SFML/Graphics.hpp>
 
+#include <iostream>
+
+using namespace std;
 
 class PauseGame {
+public:
+    PauseGame();
+
+    ~PauseGame();
+
+    void drawPaus(sf::RenderWindow &window);
+
+    sf::Font font;
+
+    sf::Text pause;
 
 };
 
-
-#endif //INC_1_PAUSEGAME_H
+#endif //PROJECT1_PAUSEGAME_H
